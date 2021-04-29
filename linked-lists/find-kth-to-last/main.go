4b0d716084d8d8fcaeb5fc0b76ab29ee7788d13f
Aled Davies <aledsdavies@gmail.com>
2021-04-29 11:16:23 +0100

    Looked at the solutions and tweaked mine

--- linked-lists/find-kth-to-last/main.go
+++ linked-lists/find-kth-to-last/main.go
@@ -11,10 +11,19 @@ func main() {
 /*
 	Attempt 1
 
-	This may be easier because of my implementation of linked list but I can do a check on the length without the length
-	it could still be done in O(N) time by looping by keeping an eye on the currNode and ensuring that it isn't nil.
+	My implementation of linked list has the length so this becomes an O(N) time problem with no additional space. After
+	reviewing the solutions you probably wouldn't have the length because it makes solving this problem trivial.
 
-	//
+	After reviewing the answer I misunderstood the question and am returning the wrong values I have altered the code below
+	to handle the new information.
+
+	There are several approaches to implementing this without the length. Some use recursion and others return different
+	values or print out the node once it is found.
+
+	- Recursive 1 = Prints "kth to last node is head.data" and returns the integer index where the node was found
+	- Recursive 2 = where we return the pointer to the node
+	- Recursive 3 = We use a wrapper class to iterate over the nodes
+	- Iterative 1 = This returns the node without the length
 
 
 */
@@ -26,8 +35,9 @@ func FindKthToLast_Attempt1(k int, sll list.SinglyLinkedList) int {
 	}
 
 	currNode := sll.Head
-	counter := 1
-	for counter != k {
+	index := sll.Length - k
+	counter := 0
+	for counter < index {
 		currNode = currNode.Next
 		counter++
 	}
--- linked-lists/find-kth-to-last/main_test.go
+++ linked-lists/find-kth-to-last/main_test.go
@@ -17,8 +17,8 @@ func TestFindKthToLast_Attempt1(t *testing.T) {
 		want int
 	}{
 		{name: "Test Case 1", args: args{sll: *list.NewSinglyLinkedList(), k: 5}, want: 0},
-		{name: "Test Case 2", args: args{sll: *list.NewSinglyLinkedList(1, 1), k: 2}, want: 1},
-		{name: "Test Case 3", args: args{sll: *list.NewSinglyLinkedList(1, 2, 3, 4, 1, 5, 6), k: 5}, want: 1},
+		{name: "Test Case 2", args: args{sll: *list.NewSinglyLinkedList(2, 1), k: 2}, want: 2},
+		{name: "Test Case 3", args: args{sll: *list.NewSinglyLinkedList(1, 2, 3, 4, 1, 5, 6), k: 5}, want: 3},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {

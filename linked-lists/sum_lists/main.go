5457e08b2671f1138415e404f8fac9e06287ff79
Aled Davies <aledsdavies@gmail.com>
2021-05-05 15:19:36 +0100

    Did part b of the sum list question

--- linked-lists/sum_lists/main.go
+++ linked-lists/sum_lists/main.go
@@ -28,27 +28,88 @@ func SumListReverse_Attempt1(list1, list2 linked_lists.SinglyLinkedList) linked_
 	carry := 0
 
 	for lhsHead != nil {
-			// Add lhs to rhs
-			addResult := lhsHead.Data + rhsHead.Data + carry
+		// Add lhs to rhs
+		addResult := lhsHead.Data + rhsHead.Data + carry
 
-			// We can work out the carry by testing if the number is greater than 10
-			if addResult > 10 && lhsHead.Next == nil {
-				// If its the last value we need to handle it differently
-				addResult++
-				sum = append(sum, addResult%10)
-				sum = append(sum, 1)
-				break
-			} else if addResult > 10 {
-				carry = 1
-			} else {
-				carry = 0
-			}
-
-			// Modulo 10 will give us the value we want
+		// We can work out the carry by testing if the number is greater than 10
+		if addResult > 10 && lhsHead.Next == nil {
+			// If its the last value we need to handle it differently
+			addResult++
 			sum = append(sum, addResult%10)
-			lhsHead = lhsHead.Next
-			rhsHead = rhsHead.Next
+			sum = append(sum, 1)
+			break
+		} else if addResult > 10 {
+			carry = 1
+		} else {
+			carry = 0
+		}
+
+		// Modulo 10 will give us the value we want
+		sum = append(sum, addResult%10)
+		lhsHead = lhsHead.Next
+		rhsHead = rhsHead.Next
 	}
 
 	return *linked_lists.NewSinglyLinkedList(sum...)
+}
+
+/*
+	Attempt 1
+
+	This is more complex than the above because we can no longer just add the carry. This solution works
+	largely the same as the above with an additional step after adding the numbers to handle the carry
+
+	The solution they use takes a different approach using objects to pass on the carry. It also once again uses recursion
+	to to sum all the values. This time the recursion returns the partial sum with the object and this contains the carry
+	the returned carry can then be added and passed to a new linked list
+*/
+func SumListForward_Attempt1(list1, list2 linked_lists.SinglyLinkedList) linked_lists.SinglyLinkedList {
+	var lhs linked_lists.SinglyLinkedList
+	var rhs linked_lists.SinglyLinkedList
+	if list1.Length > list2.Length {
+		lhs = list1
+		rhs = list2
+	} else {
+		lhs = list2
+		rhs = list1
+	}
+
+	buffer := lhs.Length - rhs.Length
+	lhsHead := lhs.Head
+	rhsHead := rhs.Head
+	sum := []int{}
+
+	for lhsHead != nil {
+		if buffer > 0 {
+			sum = append(sum, lhs.Head.Data)
+		} else {
+			addResult := lhsHead.Data + rhsHead.Data
+			sum = append(sum, addResult)
+		}
+		lhsHead = lhsHead.Next
+		rhsHead = rhsHead.Next
+	}
+
+	figures := []int{}
+	carry := 0
+	for i := len(sum) - 1; i != -1; i-- {
+		val := sum[i] + carry
+
+		if val > 10 && i == 0 {
+			// If its the last value we need to handle it differently
+			val++
+			figures = append([]int{val % 10}, figures...)
+			figures = append([]int{1}, figures...)
+			break
+		} else if val > 10 {
+			carry = 1
+		} else {
+			carry = 0
+		}
+
+		// Modulo 10 will give us the value we want
+		figures = append([]int{val % 10}, figures...)
+	}
+
+	return *linked_lists.NewSinglyLinkedList(figures...)
 }
